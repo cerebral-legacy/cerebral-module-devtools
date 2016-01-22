46fb8641d90c081e125573c3703a5dfde7838f3f
Alexey Gurianov <gurianov@gmail.com>
2016-01-22 23:16:23 +0400

    feat: initial extract from cerebral core

---
+++ sr-prerelease.js
@@ -0,0 +1,4 @@
+'use strict'
+module.exports = function (pluginConfig, _ref, cb) {
+  cb(null, 'minor')
+}
---
+++ tests/index.js
@@ -0,0 +1,33 @@
+var Controller = require('cerebral')
+var Devtools = require('../')
+
+global.window = {
+  addEventListener: function () {},
+  dispatchEvent: function () {}
+}
+global.CustomEvent = function () {}
+
+var Model = function () {
+  return function () {
+    return {
+      mutators: {
+        set: function (path, value) {
+          var state = {}
+          state[path.pop()] = value
+        }
+      }
+    }
+  }
+}
+
+exports['should load devtools'] = function (test) {
+  var controller = Controller(Model({}))
+
+  console.log(controller.modules({
+    devtools: Devtools()
+  }))
+
+  console.log(controller.getServices())
+  test.equal(controller.getServices().devtools.store.getSignals().length, 0)
+  test.done()
+}
